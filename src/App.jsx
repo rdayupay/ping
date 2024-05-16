09b686dbb0cb3948cfbfeb521833f59a485f1e5f
rdayupay <raqueldayupay@gmail.com>
2024-05-17 02:09:44 +0800

    Create user store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+
 import ChatInterface from './components/ChatInterface';
 import Login from './components/Login';
 import { ToasterProvider } from './components/ToasterProvider';
+import { auth } from './lib/firebase';
+import { useUserStore } from './lib/userStore';
 
 function App() {
-  const isUser = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (authUser) => {
+      fetchUserInfo(authUser?.uid);
+    });
+
+    return () => unSubscribe();
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
-      {isUser ? <ChatInterface /> : <Login />}
+      {currentUser ? <ChatInterface /> : <Login />}
       <ToasterProvider />
     </>
   );
