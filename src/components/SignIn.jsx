b8a2278c4287e6dab7ffe1bc261b1e9b99274574
rdayupay <raqueldayupay@gmail.com>
2024-05-17 01:34:02 +0800

    Disable inputs of sign in on submit

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import toast from 'react-hot-toast';
+
+import { auth } from '../lib/firebase';
+
 function SignIn() {
-  const handleSignIn = (event) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = async (event) => {
     event.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(event.target);
+
+    const { email, password } = Object.fromEntries(formData);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -11,17 +32,20 @@ function SignIn() {
           type="text"
           placeholder="Email"
           name="email"
-          className="w-full px-4 py-2  rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2  rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:bg-gray-400"
+          disabled={loading}
         />
         <input
           type="password"
           placeholder="Password"
           name="password"
-          className="w-full px-4 py-2  rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2  rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:cursor-not-allowed disabled:bg-gray-400"
+          disabled={loading}
         />
         <button
           type="submit"
-          className="w-full bg-blue-500 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          className="w-full bg-blue-500 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400"
+          disabled={loading}
         >
           Sign In
         </button>
