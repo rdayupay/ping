0fdbe7d66c98fc02ee339d2b6bc410d5bf647bd4
rdayupay <raqueldayupay@gmail.com>
2024-05-17 11:48:38 +0800

    Add query to modal

--- src/components/AddUserModal.jsx
+++ src/components/AddUserModal.jsx
@@ -1,7 +1,32 @@
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import PropTypes from 'prop-types';
 import { X } from 'react-feather';
+import { db } from '../lib/firebase';
+import { useState } from 'react';
 
 function AddUserModal({ onClose }) {
+  const [user, setUser] = useState(null);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const username = formData.get('username');
+
+    try {
+      const userRef = collection(db, 'users');
+
+      const q = query(userRef, where('username', '==', username));
+
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data());
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative p-8">
@@ -13,7 +38,10 @@ function AddUserModal({ onClose }) {
         </button>
 
         <div className="bg-white p-8 rounded-md shadow-lg">
-          <form className="flex flex-col md:flex-row mb-6">
+          <form
+            className="flex flex-col md:flex-row mb-6"
+            onSubmit={handleSearch}
+          >
             <input
               type="text"
               placeholder="Username"
@@ -21,27 +49,29 @@ function AddUserModal({ onClose }) {
               className="text-black w-full md:w-auto px-4 py-2 rounded-md bg-slate-200 border border-gray-300 focus:ring-2 focus:ring-blue-500"
             />
             <button
-              type="button"
+              type="submit"
               className="text-sm mt-2 md:mt-0 ml-0 md:ml-2 px-4 md:w-auto bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
             >
               Search
             </button>
           </form>
 
-          <div className="flex items-center">
-            <img
-              src="./Selena.png"
-              alt="Selena Gomez smiling"
-              className="w-10 h-10 rounded-full object-cover mr-4"
-            />
-            <span className="text-md text-gray-600 font-semibold mr-2">
-              Selena Gomez
-            </span>
+          {user && (
+            <div className="flex items-center">
+              <img
+                src={user.avatar || './Selena.png'}
+                alt="Selena Gomez smiling"
+                className="w-10 h-10 rounded-full object-cover mr-4"
+              />
+              <span className="text-md text-gray-600 font-semibold mr-2">
+                {user.username}
+              </span>
 
-            <button className="text-sm ml-auto bg-violet-800 text-white px-4 py-2 rounded-md hover:bg-violet-600 focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
-              Add User
-            </button>
-          </div>
+              <button className="text-sm ml-auto bg-violet-800 text-white px-4 py-2 rounded-md hover:bg-violet-600 focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
+                Add User
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
